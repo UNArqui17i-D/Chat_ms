5cd8b2e21b076c5a494011955e33446fd88052c3
erickvelasco11 <erickvelasco11@gmail.com>
2017-03-18 12:55:00 -0500

    Hello world web GET

--- server.go
+++ server.go
@@ -1,2 +1,20 @@
 package main
 
+import (
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+func HolaMundo(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("Hola mundo"))
+}
+
+func main(){
+	mux := mux.NewRouter()
+	mux.HandleFunc("/Hola", HolaMundo).Methods("GET")
+
+	http.Handle("/", mux)
+	log.Println("El servidor encuentra en el puerto 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
