08174a205d6212781bc4d24de598b02b40c4f308
Erick Velasco <erickvelasco11@gmail.com>
2017-05-12 17:15:18 -0500

    start2

--- start2.sh
+++ start2.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env sh
 
-mvn -f pom.xml package
-
-./../rancher-compose --project-name Chat-ms \
+./../rancher-compose --project-name chat_ms \
     --url http://192.168.99.100:8080/v1/projects/1a5 \
-    --access-key A488D9A45F7630739A2D \
-    --secret-key i4X428DBgAtakraeiQfrFCeFnogt6oaUnHbBf6PS \
+    --access-key 187A10CF0B968A0BC159 \
+    --secret-key Vy68nzh4oo1raWEjqHhCHRGzcUfqqPHT7NUXQcMw \
     -f docker-compose.yml \
     --verbose up \
     -d --force-upgrade \
