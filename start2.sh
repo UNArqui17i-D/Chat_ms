d8d7f00634860fade05ff2a002feb4cabe52a6d7
Erick Velasco <erickvelasco11@gmail.com>
2017-05-12 17:26:51 -0500

    start2

--- start2.sh
+++ start2.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-./../rancher-compose --project-name chat_ms \
+./../rancher-compose --project-name chat-ms \
     --url http://192.168.99.100:8080/v1/projects/1a5 \
     --access-key 187A10CF0B968A0BC159 \
     --secret-key Vy68nzh4oo1raWEjqHhCHRGzcUfqqPHT7NUXQcMw \
