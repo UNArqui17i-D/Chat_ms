db5ec6c6b26b84dc222ae49552bfb1509e6382d2
jaquinonesg <jaquinonesg@unal.edu.co>
2017-05-26 13:56:41 -0500

    Antes de que corra la sangre

--- start2.sh
+++ start2.sh
@@ -2,8 +2,8 @@
 
 ./../rancher-compose --project-name chat-ms \
     --url http://192.168.99.100:8080/v1/projects/1a5 \
-    --access-key 187A10CF0B968A0BC159 \
-    --secret-key Vy68nzh4oo1raWEjqHhCHRGzcUfqqPHT7NUXQcMw \
+    --access-key D0C832691E321859CD8D \
+    --secret-key fjN1tMEDZRYHcB1zEuSsoKVmF7NtPahNWX3uBWE2 \
     -f docker-compose.yml \
     --verbose up \
     -d --force-upgrade \
